97384558b15f7a08e471c4af7bc54c0945b47204
SharnuSharath123 <sharnuburli143@gmail.com>
2024-06-30 22:40:09 +0530

    modules

---
+++ operations.py
@@ -0,0 +1,14 @@
+# approach 1
+import calc
+calc.add(100,300)
+calc.mul(10,30)
+
+# approach 2
+from calc import add, mul
+add(100,300)
+mul(10,30)
+
+# approach 3
+from calc import *
+add(15,25)
+mul(12,21)
